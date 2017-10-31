921c874f1e204ffe0253c7345785323c111ce8d7
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-31 16:42:40 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,14 @@
 class Shoe
-  
+
   attr_accessor :brand
   attr_accessor :color
   attr_accessor :size
   attr_accessor :material
   attr_accessor :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
 
   def cobble
     @condition = "new"
